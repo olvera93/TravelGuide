f7c384c15e96c70fc8c81a0339bb2d3f688bcd74
olvera93 <olvera9302@outlook.com>
2023-04-06 15:01:57 -0600

    [FINISH] The application was finished

---
+++ app/src/main/java/com/olvera/travelguide/home/presentation/components/HomePopularPlaceItem.kt
@@ -0,0 +1,72 @@
+package com.olvera.travelguide.home.presentation.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.olvera.travelguide.home.domain.model.Place
+import com.olvera.travelguide.home.domain.model.Region
+
+@Composable
+fun HomePopularPlaceItem(
+    modifier: Modifier = Modifier,
+    place: Place,
+    onPlaceClick: (String) -> Unit
+) {
+
+    Box(
+        modifier = modifier
+            .size(180.dp, 250.dp)
+            .clip(RoundedCornerShape(20.dp))
+            .clickable { onPlaceClick("${place.country}, ${place.city}") }
+    ) {
+        AsyncImage(
+            model = place.image,
+            contentDescription = "${place.country}, ${place.city}",
+            contentScale = ContentScale.Crop
+        )
+        Text(
+            text = "${place.country}, ${place.city}",
+            modifier = Modifier
+                .align(Alignment.BottomStart)
+                .padding(12.dp),
+            color = Color.White,
+            fontSize = 13.sp,
+            fontWeight = FontWeight.Black,
+            style = TextStyle(
+                shadow = Shadow(
+                    color = Color.Black,
+                    offset = Offset(3f,3f),
+                    blurRadius = 5f
+                )
+            )
+        )
+    }
+
+}
+
+@Preview
+@Composable
+fun HomePopularPlaceItemPreview() {
+    HomePopularPlaceItem(
+        place = Place("Argentina", "Buenos Aires", Region.AMERICA, ""),
+        onPlaceClick = {}
+    )
+
+}
